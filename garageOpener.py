7a9e571f5045d0c7893085a165f2d8d41d640b8c
Rylan Sturm <rylan.sturm@gmail.com>
2023-02-01 22:04:02 -0700

    garageOpener.py, run.sh, and .gitignore

---
+++ garageOpener.py
@@ -0,0 +1,24 @@
+from gpiozero import LED
+import sys
+from time import sleep
+
+DOORS = {
+    1: 21,
+    2: 20,
+    3: 26,
+    4: 16
+}
+
+def main(door):
+    print(door, ' on.')
+    sleep(.5)
+    print(door, ' off.')
+
+
+if __name__ == '__main__':
+    try:
+        door = int(sys.argv[1])
+        door = DOORS[door]
+        main(door)
+    except:
+        raise ValueError("The argument passed to garageOpener.py was not an integer")
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+~/pers/GaragePi/venv/bin/python3 ~/pers/GaragePi/garageOpener.py 2
