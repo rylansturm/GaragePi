2f17b1788c7dfc9e08d9b78e0c69887668a28e7f
Rylan Sturm <rylan.sturm@gmail.com>
2023-07-10 12:19:08 -0600

    working Rust script. Needs validation and error handling

--- garage/src/main.rs
+++ garage/src/main.rs
@@ -3,24 +3,89 @@ use std::thread;
 use std::time::Duration;
 use std::env;
 
+const DOOR1RELAY: u8 = 22;
+const DOOR1SENSOR: u8 = 27;
+const DOOR2RELAY: u8 = 23;
+const DOOR2SENSOR: u8 = 17;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    match args[1].as_str() {
-        "1" | "2" => println!("Valid"),
-        _ => {println!("Invalid doornum"); return},
+    if !check_args(&args) {
+    } else {
+        let command = &args[1];
+        let door = &args[2];
+
+        match command.as_str() {
+            "open" => open(door),
+            "close" => close(door),
+            "check" => check(door),
+            _ => println!("invalid command \"{}\".", command.as_str()),
+        }
+/*
+        let doornum: u8 = match &args[1] {
+            "1" => DOOR1RELAY,
+            "2" => DOOR2RELAY,
+            _ => 6,
+        };
+
+        let led = LED::new(doornum);
+        led.off();
+        println!("{doornum} on");
+        thread::sleep(Duration::from_millis(250));
+        led.on();
+        println!("{doornum} off");
+*/
     }
+}
 
-    let doornum: u8 = match args[1].parse().unwrap() {
-        1 => 12,
-        2 => 16,
-        _ => 6,
+fn check_args(args: &Vec<String>) -> bool {
+    true
+}
+
+fn open(door: &String) {
+    if !status(door) {
+        println!("opening door {}", door);
+        press(&door);
+    } else {
+        println!("door {} is already open", door);
+    }
+}
+
+fn close(door: &String) {
+    if status(door) {
+        println!("closing door {}", door);
+        press(&door);
+    } else {
+        println!("door {} is already closed", door);
+    }
+}
+
+fn check(door: &String) {
+    let status: String = match status(door) {
+        false => String::from("closed"),
+        true => String::from("open"),
     };
+    println!("door {} is {}", door, status);
+}
 
-    let led = LED::new(doornum);
-    led.off();
-    println!("{doornum} on");
+fn press(door: &String) {
+    let doorpin: u8 = match door.as_str() {
+        "1" => DOOR1RELAY,
+        "2" => DOOR2RELAY,
+        _ => 0,
+    };
+    let door = LED::new(doorpin);
+    door.on();
     thread::sleep(Duration::from_millis(250));
-    led.on();
-    println!("{doornum} off");
+    door.off();
+}
+
+fn status(door: &String) -> bool {
+    let doorpin: u8 = match door.as_str() {
+        "1" => DOOR1SENSOR,
+        "2" => DOOR2SENSOR,
+        _ => 0,
+    };
+    InputDevice::new(doorpin).value()
 }
